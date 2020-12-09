5b3f1dfe2e2062784aabc0ae1ee17d598d284123
Stephen Jenkins <stephen.jenkins@nice.com>
2020-12-09 16:46:11 +0000

    Initial commit with Day 1 challenge response implementation

---
+++ AdventOfCode2020/AdventOfCode2020/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AdventOfCode2020.Challenges;
+
+namespace AdventOfCode2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dayOne = new DayOne();
+            long result = dayOne.GetChallengeResult();
+            Console.WriteLine("Day one challenge result= "+result);
+        }
+    }
+}
